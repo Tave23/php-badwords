2c9427b04481aa5c1b393bdd4b5b424ba7cf0295
Davide Tavella <tavella01@gmail.com>
2021-12-17 14:49:39 +0100

    paragrafo random

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<!-- Creare una variabile con un paragrafo di testo a vostra scelta.
+Stampare a schermo il paragrafo e la sua lunghezza.
+Una parola da censurare viene passata dall’utente tramite parametro GET.
+Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
+
+
+<?php
+
+$paragrafo = "Lorem ipsum, dolor sit amet consectetur adipisicing badword. Eaque saepe illum itaque iste dolore repellat veniam, voluptatem badword neque incidunt unde soluta amet dolorum badword, odit adipisci doloribus. Ex badword nihil hic aspernatur sequi labore nobis, officia ab voluptatum id badword aliquid molestiae blanditiis quidem in badword fugit ratione est?"
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +21,8 @@
    <title>php-badwords</title>
 </head>
 <body>
+
+   
    
 </body>
 </html>
